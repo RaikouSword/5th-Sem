8cf47b06199db8e654173b1ff4a0376b4814168e
RaikouSword <raikouashensword@gmail.com>
2025-09-09 13:03:13 +0530

    tities

---
+++ PPWC/hello.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+#define PI 3.141
+
+int main(){
+    int rad = 2;
+    int area = PI * rad  * rad
+    printf("Area of circle is: %f \n", area)
+    return 0;
+}
---
+++ PPWC/math.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define PI 3.141
+int main() {
+    int rad = 2;
+    float area = PI * rad * rad;
+    printf("Area of Circle is: %f ", area); 
+
+    return 0;
+}
