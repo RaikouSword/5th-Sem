c1ec149335d167262a1a9b054189a8d3cc5a6c43
RaikouSword <raikouashensword@gmail.com>
2025-10-28 12:54:25 +0530

    oct28

---
+++ PPWC/stringPointer.c
@@ -0,0 +1,17 @@
+char st[100];
+char *p=st;
+printf("Enter a string: ");
+fgets(p,100,stdin);
+printf("Entered string: ");
+fputs(s,stdout);
+
+
+
+
+char st[100] = "fggg";
+char *p = st;
+printf("String: ");
+fputs(p,stdout);
+char *p1 = "fggg";
+printf("String: ");
+fputs(p1,stdout);
